0a9a0c3b827a3c624c77b09261305513ae193a1b
Matthew Beale <matt.beale@madhatted.com>
2015-03-26 18:43:52 -0400

    WIP: passing tests with changeset

--- app/organization/members/edit/route.js
+++ app/organization/members/edit/route.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import Changeset from "diesel/utils/changeset";
 
 export default Ember.Route.extend({
   model(params){
@@ -12,5 +13,43 @@ export default Ember.Route.extend({
   setupController(controller, model){
     controller.set('model', model);
     controller.set('organization', this.modelFor('organization'));
+    let changeset = Changeset.create({
+      key(keyData) {
+        return keyData.organizationRole.get('id');
+      },
+      initialValue(keyData) {
+        const user = keyData.user;
+        const organizationRole = keyData.organizationRole;
+        return user.get('roles').contains(organizationRole);
+      }
+    });
+    controller.set('changeset', changeset);
+  },
+
+
+  actions: {
+    save() {
+      const promises = [];
+      const changeset = this.controller.get('changeset');
+
+      changeset.forEachValue((keyData, initialValue, value) => {
+        if (initialValue === value) {
+          return;
+        }
+
+        if (value) {
+          promises.push(this.store.createRecord('membership', {
+            user: this.currentModel.get('data.links.self'),
+            role: keyData.organizationRole
+          }).save());
+        } else {
+          //crazy stuff
+        }
+      });
+
+      Ember.RSVP.all(promises).then(() => {
+        this.transitionTo('organization.members');
+      });
+    }
   }
 });
